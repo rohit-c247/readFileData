c8b0dfd6786e821d8d528cc71d6eda0139b0a333
rohitachapter247 <rohit.a@chapter247.com>
2022-04-26 20:02:22 +0530

    read file code

--- app/Jobs/ReadFile.php
+++ app/Jobs/ReadFile.php
@@ -33,47 +33,5 @@ class ReadFile implements ShouldQueue
     public function handle()
     {
         Excel::import(new ImportFileData, public_path() . "/Sample-Spreadsheet-10-rows.csv");
-
-        /*$filename = public_path() . "/Sample-Spreadsheet-10-rows.csv";
-        $delimiter = ',';
-        if (!file_exists($filename) || !is_readable($filename)) {
-            return false;
-        }
-        $header = null;
-        $data = array();
-        if (($handle = fopen($filename, 'r')) !== false) {
-            while (($row = fgetcsv($handle, 1000, $delimiter)) !== false) {
-                if (!$header) {
-                    $header = $row;
-                } else {
-                    $data[] = array_combine($header, $row);
-                }
-            }
-            fclose($handle);
-        }
-
-        foreach ($data as $val) {
-            FileData::insert($val);
-        }*/
-    }
-
-    public function csvToArray($filename = '', $delimiter = ',')
-    {
-        if (!file_exists($filename) || !is_readable($filename)) {
-            return false;
-        }
-        $header = null;
-        $data = array();
-        if (($handle = fopen($filename, 'r')) !== false) {
-            while (($row = fgetcsv($handle, 1000, $delimiter)) !== false) {
-                if (!$header) {
-                    $header = $row;
-                } else {
-                    $data[] = array_combine($header, $row);
-                }
-            }
-            fclose($handle);
-        }
-        return $data;
     }
 }
